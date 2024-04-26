81d22c76fd3ada343bdbfedcf0791aea0776cb7e
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-04-26 15:00:50 +0200

    ticker

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+/* functions for isolated margin account api */
+async function getTickerPrice(symbol) {
+    try {
+        const priceFetch = await fetch(`http://binance.com/api/v3/ticker/price?symbol=${symbol}`);
+        if (!priceFetch.ok) {
+            throw new Error(`Failed to fetch price for symbol ${symbol}: ${priceFetch.statusText}`);
+        }
+        const priceBody = await priceFetch.json();
+        if (!priceBody.price) {
+            throw new Error(`Price data not found for symbol ${symbol}`);
+        }
+        return parseFloat(priceBody.price);
+    }
+    catch (error) {
+        console.log("Error fetching ticker price", error);
+        throw error;
+    }
+}
+export { getTickerPrice };
